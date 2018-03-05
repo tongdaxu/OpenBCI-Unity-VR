7b3df502c6853d8d9f8dda92950266b4747c4b34
matonglidewazi <matonglidewazi@qq.com>
2018-03-05 17:54:58 +0800

    Signed-off-by: matonglidewazi <matonglidewazi@qq.com>

---
+++ OpenBCI_Unity_Port/Assets/Scripts/EmotionEmitter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionEmitter : MonoBehaviour {
+
+	public ParticleSystem system;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		system.Emit (new ParticleSystem.EmitParams (){ position = Random.onUnitSphere }, 1);
+		
+	}
+}
